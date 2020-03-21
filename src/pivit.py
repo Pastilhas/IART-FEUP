cd110c47f9f1de93430e2d75ceca2a09ba6009d1
Pastilhas <joao.campos1221@gmail.com>
2020-03-21 14:51:58 +0000

    print finished

--- src/pivit.py
+++ src/pivit.py
@@ -9,7 +9,13 @@ class Piece:
     x: int
     y: int
     player: str
+    direction: str = 'y'
     type: str = 'minion'
+
+class bcolors:
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    ENDC = '\033[0m'
 
 ##################################################
 
@@ -21,18 +27,18 @@ def generateBoard(side):
     board.append(Piece(2, 0, 'B'))
     board.append(Piece(3, 0, 'A'))
     board.append(Piece(4, 0, 'B'))
-    board.append(Piece(0, 1, 'B'))
-    board.append(Piece(0, 2, 'A'))
-    board.append(Piece(0, 3, 'B'))
-    board.append(Piece(0, 4, 'A'))
+    board.append(Piece(0, 1, 'B', 'x'))
+    board.append(Piece(0, 2, 'A', 'x'))
+    board.append(Piece(0, 3, 'B', 'x'))
+    board.append(Piece(0, 4, 'A', 'x'))
     board.append(Piece(1, 5, 'A'))
     board.append(Piece(2, 5, 'B'))
     board.append(Piece(3, 5, 'A'))
     board.append(Piece(4, 5, 'B'))
-    board.append(Piece(5, 1, 'B'))
-    board.append(Piece(5, 2, 'A'))
-    board.append(Piece(5, 3, 'B'))
-    board.append(Piece(5, 4, 'A'))
+    board.append(Piece(5, 1, 'B', 'x'))
+    board.append(Piece(5, 2, 'A', 'x'))
+    board.append(Piece(5, 3, 'B', 'x'))
+    board.append(Piece(5, 4, 'A', 'x'))
   if side == 8:
     board.append(Piece(1, 0, 'A'))
     board.append(Piece(2, 0, 'B'))
@@ -40,24 +46,24 @@ def generateBoard(side):
     board.append(Piece(4, 0, 'A'))
     board.append(Piece(5, 0, 'B'))
     board.append(Piece(6, 0, 'A'))
-    board.append(Piece(0, 1, 'B'))
-    board.append(Piece(0, 2, 'A'))
-    board.append(Piece(0, 3, 'B'))
-    board.append(Piece(0, 4, 'B'))
-    board.append(Piece(0, 5, 'A'))
-    board.append(Piece(0, 6, 'B'))
+    board.append(Piece(0, 1, 'B', 'x'))
+    board.append(Piece(0, 2, 'A', 'x'))
+    board.append(Piece(0, 3, 'B', 'x'))
+    board.append(Piece(0, 4, 'B', 'x'))
+    board.append(Piece(0, 5, 'A', 'x'))
+    board.append(Piece(0, 6, 'B', 'x'))
     board.append(Piece(1, 7, 'A'))
     board.append(Piece(2, 7, 'B'))
     board.append(Piece(3, 7, 'A'))
     board.append(Piece(4, 7, 'A'))
     board.append(Piece(5, 7, 'B'))
     board.append(Piece(6, 7, 'A'))
-    board.append(Piece(7, 1, 'B'))
-    board.append(Piece(7, 2, 'A'))
-    board.append(Piece(7, 3, 'B'))
-    board.append(Piece(7, 4, 'B'))
-    board.append(Piece(7, 5, 'A'))
-    board.append(Piece(7, 6, 'B'))
+    board.append(Piece(7, 1, 'B', 'x'))
+    board.append(Piece(7, 2, 'A', 'x'))
+    board.append(Piece(7, 3, 'B', 'x'))
+    board.append(Piece(7, 4, 'B', 'x'))
+    board.append(Piece(7, 5, 'A', 'x'))
+    board.append(Piece(7, 6, 'B', 'x'))
 
   return board
 
@@ -83,7 +89,21 @@ def printBoard(board,side):
       if len(piece) == 0:
         line += ' '
       else:
-        line += 'x'
+        if piece[0].direction == 'y':
+          if piece[0].type == 'minion':
+            char = '|'
+          else:
+            char = '\u256B'
+        else:
+          if piece[0].type == 'minion':
+            char = '\u2500'
+          else:
+            char = '\u256A'
+
+        if piece[0].player == 'A':
+          line += bcolors.BLUE + char + bcolors.ENDC
+        else:
+          line += bcolors.GREEN + char + bcolors.ENDC
       board = [p for p in board if not (p.x == x and p.y == y)]
       line += separatorRt
       x += 1
