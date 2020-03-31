8340c039e3cb8c438079243e1b0719619024551f
up201504695 <simao.p.m.santos@gmail.com>
2020-03-31 23:48:11 +0100

    made board more legible #31

--- src/Pivit.java
+++ src/Pivit.java
@@ -276,11 +276,15 @@ public class Pivit {
 
 	// prints board
 	public void printBoard() {
-		String separatorLine = "+---+---+---+---+---+---+";
+		System.out.println();
+		System.out.println("   | 0 | 1 | 2 | 3 | 4 | 5 |");
+
+		String separatorLine = "   +---+---+---+---+---+---+";
 		String separatorLeft = "| ";
 		String separatorRight = " ";
 		if (this.size == 8) {
 			separatorLine += "---+---+";
+			System.out.println("| 6 | 7 |");
 		}
 
 		for (int y = 0; y < this.size; y++) {
@@ -306,9 +310,10 @@ public class Pivit {
 				line += separatorRight;
 			}
 			line += separatorLeft;
-			System.out.println(line);
+			System.out.println(y + "  "+ line);
 		}
 		System.out.println(separatorLine);
+		System.out.println();
 	}
 
 	public void debugBoard() {
